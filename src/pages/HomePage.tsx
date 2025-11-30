c27a5fc67fb192093d9977b204a4f49ba03ee69b
chrisp-png <chris.p@allphasesfl.com>
2025-11-30 16:23:34 -0500

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,78 +9,10 @@ import AuthorityGroup from '../components/authority/AuthorityGroup';
 import ProjectsGallery from '../components/home/ProjectsGallery';
 import TestimonialsSection from '../components/home/TestimonialsSection';
 import BottomCTA from '../components/home/BottomCTA';
+import HomeLocalBusinessSchema from '../components/HomeLocalBusinessSchema';
 import RoofGuideFaqSchema from '../components/RoofGuideFaqSchema';
 
 export default function HomePage() {
-  const localBusinessSchema = {
-    "@context": "https://schema.org",
-    "@type": "RoofingContractor",
-    "name": "All Phase Construction USA, LLC",
-    "url": "https://allphaseusa.com",
-    "telephone": "754-227-5605",
-    "email": "info@allphaseusa.com",
-    "address": {
-      "@type": "PostalAddress",
-      "streetAddress": "590 Goolsby Blvd",
-      "addressLocality": "Deerfield Beach",
-      "addressRegion": "FL",
-      "postalCode": "33442",
-      "addressCountry": "US"
-    },
-    "areaServed": [
-      {
-        "@type": "State",
-        "name": "Florida"
-      },
-      {
-        "@type": "AdministrativeArea",
-        "name": "Broward County"
-      },
-      {
-        "@type": "AdministrativeArea",
-        "name": "Palm Beach County"
-      },
-      {
-        "@type": "City",
-        "name": "Boca Raton"
-      },
-      {
-        "@type": "City",
-        "name": "Deerfield Beach"
-      },
-      {
-        "@type": "City",
-        "name": "Coral Springs"
-      },
-      {
-        "@type": "City",
-        "name": "Coconut Creek"
-      },
-      {
-        "@type": "City",
-        "name": "Delray Beach"
-      },
-      {
-        "@type": "City",
-        "name": "Pompano Beach"
-      },
-      {
-        "@type": "City",
-        "name": "Fort Lauderdale"
-      },
-      {
-        "@type": "City",
-        "name": "West Palm Beach"
-      }
-    ],
-    "description": "Dual-licensed Florida General Contractor and Roofing Contractor specializing in tile, metal, shingle, and flat roofs in South Florida.",
-    "geo": {
-      "@type": "GeoCoordinates",
-      "latitude": 26.314,
-      "longitude": -80.099
-    }
-  };
-
   return (
     <>
       <SEO
@@ -89,8 +21,8 @@ export default function HomePage() {
         url="https://chrisp-png-roofing-c-gxj0.bolt.host/"
         canonical="https://chrisp-png-roofing-c-gxj0.bolt.host/"
         ogImage="https://chrisp-png-roofing-c-gxj0.bolt.host/og-image.jpg"
-        schemaJson={localBusinessSchema}
       />
+      <HomeLocalBusinessSchema />
       <RoofGuideFaqSchema />
       <HeroSection />
       <TrustBadges />
