5f853f489d822775b754c2223c285d47b5ed726e
chrisp-png <chris.p@allphasesfl.com>
2025-12-01 11:53:31 -0500

    Deleted App.tsx

--- src/pages/RoofCostBoyntonBeachPage.tsx
+++
@@ -1,5 +0,0 @@
-import CityRoofCostPage from './CityRoofCostPage';
-
-export default function RoofCostBoyntonBeachPage() {
-  return <CityRoofCostPage cityName="Boynton Beach" citySlug="boynton-beach" isCoastal={true} />;
-}
